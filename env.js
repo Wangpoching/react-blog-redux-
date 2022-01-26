0e1b28f23a26a471555fdc9e71043344ad2a9bb2
Peter Wang <wangpeter588@gmail.com>
2022-01-27 01:47:19 +0800

    Updates

---
+++ env.js
@@ -0,0 +1,9 @@
+window.env = {
+  "API_KEY": "AIzaSyDwEK2eVoaxdegcuZ-Qo04zI9TER4NIkhY",
+  "AUTH_DOMAIN": "react-blog-78cfd.firebaseapp.com",
+  "PROJECT_ID": "react-blog-78cfd",
+  "STORAGE_BUCKET": "react-blog-78cfd.appspot.com",
+  "MESSAGINGSENDER_ID": "307157674489",
+  "APP_ID": "1:307157674489:web:46c0e4a3486a73cb273528",
+  "MEASUREMENT_ID": "G-B38KZEEWMZ"
+};
